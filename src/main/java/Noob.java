b536c84e84806fa2d763d0c676ac68fd9c10432a
zzdlklzz <daniel.loo.kang.le@gmail.com>
2025-08-19 13:17:02 +0800

    Implement Level-0

---
+++ src/main/java/Noob.java
@@ -0,0 +1,14 @@
+public class Noob {
+    public static void main(String[] args) {
+        Noob bot = new Noob();
+        bot.greet();
+    }
+
+    public void greet() {
+        System.out.println("Hello! I'm Noob\nWhat can I do for you?");
+    }
+
+    public void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
