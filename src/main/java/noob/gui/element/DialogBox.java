646961b626de9e60e9d2fed762ae5752b6152c7e
zzdlklzz <daniel.loo.kang.le@gmail.com>
2025-09-03 17:48:19 +0800

    Update DialogBox to put Noob response dialog box to left side

--- src/main/java/noob/gui/element/DialogBox.java
+++ src/main/java/noob/gui/element/DialogBox.java
@@ -1,6 +1,9 @@
 package noob.gui.element;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -10,7 +13,7 @@ public class DialogBox extends HBox {
     private Label text;
     private ImageView imageView;
 
-    public DialogBox(String text, Image image) {
+    private DialogBox(String text, Image image) {
         this.text = new Label(text);
         this.imageView = new ImageView(image);
 
@@ -22,5 +25,25 @@ public class DialogBox extends HBox {
         this.setAlignment(Pos.TOP_RIGHT);
 
         this.getChildren().addAll(this.text, this.imageView);
+    }
+
+    public static DialogBox getUserDialogBox(String s, Image i) {
+        return new DialogBox(s, i);
+    }
+
+    public static DialogBox getNoobDialogBox(String s, Image i) {
+        DialogBox dialogBox = new DialogBox(s, i);
+        dialogBox.flip();
+        return dialogBox;
+    }
+
+    /**
+     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     */
+    private void flip() {
+        this.setAlignment(Pos.TOP_LEFT);
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        FXCollections.reverse(tmp);
+        this.getChildren().setAll(tmp);
     }
 }
